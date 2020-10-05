51e778ab7b9a5e6d34b72bb5d9f82efd4c2792df
liubenwei <2544853876@qq.com>
2020-10-05 08:37:38 +0800

    add byte code

---
+++ byte-code/src/main/java/com/liu/bytecode/unit1/Myagent.java
@@ -0,0 +1,32 @@
+package com.liu.bytecode.unit1;
+
+import java.lang.instrument.Instrumentation;
+
+/**
+ * @author liu
+ * @Date 2020/10/3 15:02
+ */
+public class Myagent {
+	public static void main(String[] args) {
+		System.out.println("asdf");
+	}
+	/**
+	 * jvm首先尝试在代理类上调用该方法
+	 * 
+	 * @param agentArgs
+	 * @param instrumentation
+	 */
+
+	public static void premain(String agentArgs,
+			Instrumentation instrumentation) {
+		System.out.println("hi! javaagent:" + agentArgs);
+	}
+
+	/**
+	 * 如果代理类上没有实现上面的方法 那么jvm会调用这个方法
+	 */
+
+	public static void premain(String agentArgs) {
+		System.out.println("no agent :" + agentArgs);
+	}
+}
---
+++ byte-code/src/test/java/com/liu/bytecode/unit1/MyagentTest.java
@@ -0,0 +1,19 @@
+package com.liu.bytecode.unit1;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+import java.lang.instrument.Instrumentation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author liu
+ * @Date 2020/10/3 15:02
+ */
+class MyagentTest {
+    public static void main(String[] args) {
+        System.out.println("asdf");
+    }
+
+}
